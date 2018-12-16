372486bf79715251733246e50ec3d7abb6f58d2e
Alyona <egorkina.alyona@gmail.com>
2018-12-16 22:25:09 +0300

    First try

---
+++ Week2_monthly_deals/main.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+#include <algorithm>
+#include <set>
+
+using namespace std;
+
+set<string> BuildMapValuesSet(const map<int, string>& m) {
+	set<string> result;
+	for (const auto& item : m) {
+		result.insert(item.second);
+	}
+	return result;
+}
+
+void ToLow(string& word) {
+	for (auto& i : word) {
+		i = tolower(i);
+	}
+}
+
+bool CompStrings(string x1, string x2) {
+	ToLow(x1);
+	ToLow(x2);
+	return x1 < x2;
+}
+ 
+
+int main() {
+	int N;
+	cin >> N;
+	vector<string> v(N);
+	for (auto& i : v) {
+		cin >> i;
+	}
+	sort(begin(v), end(v), CompStrings);
+	for (const auto& i : v) {
+		cout << i << ' ';
+	}
+	cout << endl;
+
+	int afsfd;
+	cin >> afsfd;
+	return 0;
+}
